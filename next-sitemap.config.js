d296780f1ce9a85fe3d1811f91e9dc02fb6b73d9
naoP-Bit <nk02kjm3805@gmail.com>
2024-08-31 14:00:02 +0900

    google search console 向けにmetaタグ追加

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: "https://nankousai71.vercel.app/",
+    siteUrl: "https://www.nankousai71.jp/",
     generateRobotsTxt: true, // robots.txtを生成するか
     sitemapSize: 7000,　// サイトマップの分割サイズ
   };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ const kiwi_Maru = Kiwi_Maru({weight:["300","400","500"], subsets:["latin"]})
 export const metadata: Metadata = {
   title: "祭現不可能 | 第71回南高祭",
   description: "南高・南高附属中最大の行事へようこそ。南高創立70周年・附属中創立10周年を迎えた今年、歴代の南高生から引き継がれた、「みんなが仲間に みんなが楽しく みんなで創ろう」の精神を次世代へと繋ぎます。",
+  other:{
+    "google-site-verification":["0oZkaqm-Q6yVAvi6FkC6TXOroxI5c0S2dUMsxRa__uw"]
+  }
 };
 
 export default function RootLayout({
