4a8636f59b71c7e3d3564b7bc036dfa8e05a2921
naoP-Bit <nk02kjm3805@gmail.com>
2024-08-31 16:41:29 +0900

    サイトマップ更新

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    siteUrl: "https://www.nankousai71.jp/",
+    siteUrl: "https://www.nankousai71.jp",
     generateRobotsTxt: true, // robots.txtを生成するか
     sitemapSize: 7000,　// サイトマップの分割サイズ
+    autoLastmod:false,
+    generateIndexSitemap:false
   };
